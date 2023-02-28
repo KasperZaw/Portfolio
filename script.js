5aace4ffa91b6d45c0319cc2eb7fe33393ef791c
KasperZaw <119044330+KasperZaw@users.noreply.github.com>
2023-02-28 13:34:56 +0100

    initialcommit

---
+++ script.js
@@ -0,0 +1,76 @@
+gsap.to("#image-1", {
+    scrollTrigger: {
+      trigger: ".image-parallax",
+      scrub: true
+    },
+    y: -2500 /* reverse the movement */
+  });
+  
+  gsap.to("#image-2", {
+    scrollTrigger: {
+      trigger: ".image-parallax",
+      scrub: true
+    },
+    y: -4500 /* reverse the movement */
+  });
+
+  gsap.to("#image-3", {
+    scrollTrigger: {
+      trigger: ".image-parallax",
+      scrub: true
+    },
+    y: -3500 /* reverse the movement */
+  });
+
+  gsap.to("#image-4", {
+    scrollTrigger: {
+      trigger: ".image-parallax",
+      scrub: true
+    },
+    y: -2500 /* reverse the movement */
+  });
+  
+  gsap.to("#image-5", {
+    scrollTrigger: {
+      trigger: ".image-parallax",
+      scrub: true
+    },
+    y: -1200 /* reverse the movement */
+  });
+
+  gsap.to("#image-6", {
+    scrollTrigger: {
+      trigger: ".image-parallax",
+      scrub: true
+    },
+    y: -1200 /* reverse the movement */
+  });
+  
+  gsap.to("#image-7", {
+    scrollTrigger: {
+      trigger: ".image-parallax",
+      scrub: true
+    },
+    y: -2500 /* reverse the movement */
+  });
+
+  gsap.to("#image-8", {
+    scrollTrigger: {
+      trigger: ".image-parallax",
+      scrub: true
+    },
+    y: -1400 /* reverse the movement */
+  });
+  gsap.to("#skill", {
+    scrollTrigger: {
+      trigger: ".image-parallax",
+      scrub: true
+    },
+    x: 2500 /* reverse the movement */
+  });
+
+
+
+  ScrollOut({
+    targets: 'h2'
+ })
